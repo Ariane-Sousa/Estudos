3e9d82f61571edc25e591ab20b8a688eb5d3d67d
Ariane-Sousa <ariane.crisousa@gmail.com>
2022-12-01 16:10:07 -0300

    interfaces

--- bytebank_herdado/src/bytebank_herdado/Gerente.java
+++ bytebank_herdado/src/bytebank_herdado/Gerente.java
@@ -1,24 +1,7 @@
 package bytebank_herdado;
 
-// Gerente é um Funcionário, Gerente herda da class Funcionário
-public class Gerente extends Funcionario {
-	
-	private int senha;
-	
-	public Gerente() {	
-	}
-	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-	
-	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+// Gerente é um Funcionário Auntenticavel, Gerente herda da class Funcionário
+public class Gerente extends Funcionario{
 	
 	public double getBonificacao(){
 		return super.getSalario() ; // Salario vem de cima (Class Funcionario)
--- bytebank_herdado/src/bytebank_herdado/TesteFuncionario.java
+++ bytebank_herdado/src/bytebank_herdado/TesteFuncionario.java
@@ -4,6 +4,9 @@ public class TesteFuncionario {
 
 	public static void main(String[] args) {
 		
+		Cliente cliente = new Cliente();
+		cliente.setSalario(3000.00);
+		
 		Gerente ariane = new Gerente();
 		ariane.setNome("Ariane Sousa");
 		ariane.setCpf("21232323232-9");
